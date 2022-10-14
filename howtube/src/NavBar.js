00be477d658edfbffb45541fc849759b3e663090
Michelle Lim <MichelleWLim@yahoo.com>
2022-10-13 21:15:39 -0700

    Added navbar, logo, search box, profile icon

---
+++ howtube/src/NavBar.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import searchIcon from './images/SearchIcon.png';
+import logo from './images/Logo.png';
+import profile from './images/Profile.png';
+
+const NavBarStyle = {
+	'background-color': 'rgb(59,59,59)',
+	'height': '100px',
+	'width': 'auto',
+}
+
+const LogoStyle = {
+	'position':'absolute',
+	'top': '0px',
+	'left': '0px',
+	'text-align': 'left',
+	'height': '100px',
+	'display': 'inline-block',
+}
+
+const InputStyle = {
+	'height': '10px',
+	'width': '300px',
+	'margin-top': '35px',
+	'margin-bottom': '35px',
+	'padding': '10px',
+	'text-align':'center',
+}
+
+const SearchButtonStyle = {
+	'position': 'absolute',	
+	'margin-top': '35px',
+	'margin-bottom': '35px',
+	'height': '34px',
+	'width':'100px',
+}
+
+const SearchIconStyle = {
+	'position': 'relative',
+	'height': '30px'
+}
+
+const ProfileIconStyle = {
+	'position': 'absolute',
+	'top':'0px',
+	'right':'0px',
+	'height':'75px',
+	'margin-top':'12px',
+	'margin-right':'12px',
+}
+
+export class NavBar extends React.Component {
+	render() {
+		return (
+			<div style={NavBarStyle}>
+				<div style={LogoStyle}>
+					<img src={logo} alt='HowTube' style={LogoStyle}/>
+				</div>
+				<input style={InputStyle}></input>
+				<button style={SearchButtonStyle}>
+					<img src={searchIcon} alt='search' style={SearchIconStyle}/>
+				</button>
+				<img src={profile} alt='profile' style={ProfileIconStyle}/>
+			</div>
+		);
+	}
+}
