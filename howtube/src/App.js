aa05ea262b5b242c493a3f1655b16f287da614d1
jcove88 <jsmcoveney@hotmail.com>
2022-10-16 17:53:42 -0700

    Added youtube iframe

--- howtube/src/App.js
+++ howtube/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import {NavBar} from './NavBar';
 import {Categories} from './Categories';
 import './App.css';
@@ -6,22 +5,31 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-	  <NavBar/>
-    <Categories/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with HowTube
-        </a>
-      </header>
+      <div className="Flex-container">
+        <NavBar/>
+        <Categories/>
+        <body className="App-header">
+        <div className="Video">
+          <iframe width="300" height="174" src="https://www.youtube.com/embed/GLVQZ2sXtjw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <div className="Description-container">
+            <p>
+            THE UPSIDE DOWN!!! LEGO Stranger Things - Set 75810 Time-lapse Build and Review!
+            </p>
+            <div className="Description-text">
+              <p>
+              3M views • 4 years ago
+              </p>
+              <p>
+              Brick Builder
+              </p>
+              <p>
+              New Lego 2018 Hogwarts Castle Make the magic come alive at the LEGO® Harry Potter™ 71043 Hogwarts™ Castle! This highly ...
+              </p>
+            </div>
+          </div>
+        </div>
+        </body>
+      </div>
     </div>
   );
 }
