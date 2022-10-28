a7b87deb5e6f537c2efc55258b590cd4a290c014
jcove88 <jsmcoveney@hotmail.com>
2022-10-28 15:05:41 -0700

    Adding sum file for unit testing

--- howtube/src/App.test.js
+++ howtube/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const sum = require('./sum');
+
+test('adds 2 + 3 to equal 5', () => {
+  expect(sum(2, 3)).toBe(5);
 });
